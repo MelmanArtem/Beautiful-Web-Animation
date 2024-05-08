0803cedcb9e2a598b79a0f0587a0a442e4f1a280
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2024-05-09 01:50:28 +0300

    add changes

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ const video = document.querySelector('.video-background')
 const swiperText = new Swiper('.swiper', {
 	speed: 1600, /*скорость перелистывания */
 	loop: true, /* бесконечный свайпер*/
-	mousewheel: {}, /*перелистывание прокруткой */
+	mousewheel: { }, /*перелистывание прокруткой */
 	pagination: { /*настр пагинации */
 		el: '.swiper-pagination', /* элем с классом контейнера для пагинации*/
-		clickable: true, /* кликабельность точек пагинации*/
+		clickable: true /* кликабельность точек пагинации*/
 	},
 	navigation: { /**парам навигации */
 		prevEl: '.swiper-button-prev', /* предыдущ элем */
@@ -16,6 +16,14 @@ const swiperText = new Swiper('.swiper', {
 
 swiperText.on('slideChange', function() {
 	gsap.to(video, 1.6, {
-		currentTime: (video.duration / this.slides.length) * this.realIndex
+		currentTime: (video.duration / (this.slides.length - 1)) * this.realIndex,
+		ease: Power2.easeOut
 	})
 })
+
+swiperText.on('slideChangeTransitionStart', function() {
+	video.classList.add('change') /*доб класса при перелистывании*/
+}).on('slideChangeTransitionEnd', function() {
+	video.classList.remove('change')
+})
+
